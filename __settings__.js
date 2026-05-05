bfa8352f43c54d2125b5b63809499769dd0a161a
neobiyann-bit <neobiyann@gmail.com>
2026-05-05 11:26:17 +0700

    Update __settings__.js

--- __settings__.js
+++ __settings__.js
@@ -1,6 +1,6 @@
 window.ASSET_PREFIX = "https://patek.vertex3d.asia/";
 window.SCRIPT_PREFIX = "https://patek.vertex3d.asia/";
-window.SCENE_PATH = "2486024.json";
+window.SCENE_PATH = "https://patek.vertex3d.asia/2486024.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
     'alpha': false,
@@ -9,7 +9,7 @@ window.CONTEXT_OPTIONS = {
     'powerPreference': "high-performance"
 };
 window.SCRIPTS = [ 287587945, 287587946, 287587947, 287587948, 287587949, 287587950, 287587951, 287587952, 287587953, 287587954, 287587955, 287587956, 287587957, 287587958, 287587959, 287587960, 287587961, 287587962, 287587963 ];
-window.CONFIG_FILENAME = "config.json";
+window.CONFIG_FILENAME = "https://patek.vertex3d.asia/config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
     useMouse: true,
